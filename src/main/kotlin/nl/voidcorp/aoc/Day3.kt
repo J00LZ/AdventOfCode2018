7b350704b140ab71af96ad2bfe7d5941df96d076
Julius de Jeu <jpdejeu@hotmail.nl>
2018-12-03 11:59:17 +0100

    Day 3

--- src/main/kotlin/nl/voidcorp/aoc/Day3.kt
+++ src/main/kotlin/nl/voidcorp/aoc/Day3.kt
@@ -1,11 +1,74 @@
 package nl.voidcorp.aoc
 
-class Day3: DayBase(3) {
+class Day3 : DayBase(3) {
     override fun part1(list: List<String>): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val items = list.map { Item.new(it) }
+        val arr = MutableList(1000) { MutableList(1000) { 0 } }
+        for (item in items) {
+
+            for (x in item.x until item.coords.first+item.x) {
+                for (y in item.y until item.coords.second+item.y) {
+                    arr[y][x] += 1
+                    //println("$x,$y=${arr[y][x]}")
+                }
+            }
+        }
+
+        var count = 0
+
+        for (r in arr){
+            for (c in r){
+                if (c>1){
+                    count++
+                }
+            }
+        }
+
+        return count.toString()
     }
 
     override fun part2(list: List<String>): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val items = list.map { Item.new(it) }
+        val arr = MutableList(1000) { MutableList(1000) { 0 } }
+        for (item in items) {
+
+            for (x in item.x until item.coords.first+item.x) {
+                for (y in item.y until item.coords.second+item.y) {
+                    arr[y][x] += 1
+                    //println("$x,$y=${arr[y][x]}")
+                }
+            }
+        }
+
+        for (item in items) {
+            var isbig=0
+            for (x in item.x until item.coords.first+item.x) {
+                for (y in item.y until item.coords.second+item.y) {
+                    if(arr[y][x]>1){
+                        isbig++
+                    }
+                    //println("$x,$y=${arr[y][x]}")
+                }
+            }
+            if (isbig==0){
+                return item.id.toString()
+            }
+        }
+
+        return ""
+    }
+
+    data class Item(val id: Int, val x: Int, val y: Int, val coords: Pair<Int, Int>) {
+        companion object {
+            fun new(string: String): Item {
+                val key = string.split("@").map { it.trim() }
+                val m = key.last().trim().split(':').map { it.trim() }
+
+                return Item(
+                    key.first().trim().substring(1).toInt(),
+                    m.first().split(",").first().toInt(), m.first().split(",").last().toInt(),
+                    m.last().split("x").map { it.toInt() }.let { it.first() to it.last() })
+            }
+        }
     }
 }
--- src/main/kotlin/nl/voidcorp/aoc/GenDayFiles.kt
+++ src/main/kotlin/nl/voidcorp/aoc/GenDayFiles.kt
@@ -1,6 +1,7 @@
 package nl.voidcorp.aoc
 
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 @Suppress("UNCHECKED_CAST")
 fun main(args: Array<String>) {
@@ -8,6 +9,10 @@ fun main(args: Array<String>) {
     val date = GregorianCalendar(Locale.forLanguageTag("NL-nl"))[GregorianCalendar.DAY_OF_MONTH]
     val b = Class.forName("nl.voidcorp.aoc.Day$date") as Class<DayBase>
     val c = b.getConstructor().newInstance()
+    val t = System.nanoTime()
     c.run(getText(date))
+    val q = System.nanoTime()-t
+    println("ran in ${TimeUnit.MILLISECONDS.convert(q,TimeUnit.NANOSECONDS)}")
+
 }
 
